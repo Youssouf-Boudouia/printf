8f937d3ff73fcad0c413f120ecf73c9ceef0762e
Youssouf BOUDOUIA <yboudoui@student.42.fr>
2022-05-18 06:34:01 +0200

    save update

--- render/include/libftprintf.h
+++ render/include/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: yboudoui <yboudoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 12:58:48 by yboudoui          #+#    #+#             */
-/*   Updated: 2022/05/12 10:29:05 by yboudoui         ###   ########.fr       */
+/*   Updated: 2022/05/16 15:35:37 by yboudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
+typedef struct	s_str {
+	char	*start;
+	size_t	len;
+}	t_str;
+
 typedef char *(t_flag_handler)(void*);
+typedef t_str (t_format_handler)(char **);
 
 int	ft_printf(const char *format, ...);
 
